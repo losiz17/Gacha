b3e1cb24609bdf17728cd9e57c54171c1b883e8c
losiz17 <riokam.11.12@docomo.ne.jp>
2018-03-19 00:18:38 +0900

    first commit

---
+++ Gacha/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  Gacha
+//
+//  Created by 吉川莉央 on 2018/03/18.
+//  Copyright © 2018年 RioYoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    //一番後ろの背景画像
+    @IBOutlet var haikeiImageView: UIImageView!
+    
+    //モンスターを表示する画像
+    @IBOutlet var monsterImageView: UIImageView!
+    
+    //モンスター画像を保存しておく配列
+    var monsterArray: [UIImage]!
+
+}
--- Gacha/ViewController.swift
+++ Gacha/ViewController.swift
@@ -13,11 +13,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //0~9でランダムに数字を発生させる
+        let number = Int(arc4random_uniform(10))
+        
+        //モンスターの画像を10枚追加する
+        monsterArray = [UIImage(named:"monster001.png")!,
+                        UIImage(named:"monster002.png")!,
+                        UIImage(named:"monster003.png")!,
+                        UIImage(named:"monster004.png")!,
+                        UIImage(named:"monster005.png")!,
+                        UIImage(named:"monster006.png")!,
+                        UIImage(named:"monster007.png")!,
+                        UIImage(named:"monster008.png")!,
+                        UIImage(named:"monster009.png")!,
+                        UIImage(named:"monster010.png")!]
+        //ランダムに選んだモンスターを表示させる
+        monsterImageView.image = monsterArray[number]
+        
+        if number == 9{
+            haikeiImageView.image = UIImage(named: "bg_gold.png")
+            
+        }else if number > 6{
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func gacha(){
+        self.performSegue(withIdentifier: "result", sender: nil)
     }
 
 
